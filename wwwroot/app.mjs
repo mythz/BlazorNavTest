c9c18816d47565acc5761cca797646b15745d392
Demis Bellot <demis.bellot@gmail.com>
2023-10-27 11:15:29 +0800

    Add template enhancedload test pages

---
+++ wwwroot/app.mjs
@@ -0,0 +1,12 @@
+export function load() {
+    document.querySelectorAll('[data-load]').forEach(content => {
+        const template = document.querySelector(content.getAttribute('data-load'))
+        console.log('load', content.id, template?.innerHTML ?? `template missing`)
+        if (template && content) {
+            content.innerHTML = template.innerHTML
+        }
+    })
+}
+
+document.addEventListener('DOMContentLoaded', () =>
+    Blazor.addEventListener('enhancedload', load))
